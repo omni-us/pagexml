aed4dcfefcac1176cbd8a7b22b3494a0c35d62af
Mauricio Villegas <mauricio_ville@yahoo.com>
2018-10-05 13:50:47 +0200

    Fixed docker shell scripts.

--- py-pagexml/docker/Dockerfile_build.sh
+++ py-pagexml/docker/Dockerfile_build.sh
@@ -1,4 +1,4 @@
 cd ../../
-docker build -f docker/Dockerfile_build -t pagexml-build:16.04-py27 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION= . &&
-docker build -f docker/Dockerfile_build -t pagexml-build:16.04-py35 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION=3 . &&
-docker build -f docker/Dockerfile_build -t pagexml-build:18.04-py36 --build-arg UBUNTU_TAG=18.04 --build-arg PYTHON_VERSION=3 .
+docker build -f py-pagexml/docker/Dockerfile_build -t pagexml-build:16.04-py27 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION= . &&
+docker build -f py-pagexml/docker/Dockerfile_build -t pagexml-build:16.04-py35 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION=3 . &&
+docker build -f py-pagexml/docker/Dockerfile_build -t pagexml-build:18.04-py36 --build-arg UBUNTU_TAG=18.04 --build-arg PYTHON_VERSION=3 .
