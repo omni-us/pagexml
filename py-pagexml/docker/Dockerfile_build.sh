42d2d36abceb86c9f8b96a1f74a6dbd0164c9ea9
Mauricio Villegas <mauricio_ville@yahoo.com>
2019-03-22 07:52:48 +0100

    - Updated build docker files to generate images for building wheel files in circleci. - Added circleci config to build wheel files for tags. - Fixed mistakenly dereferenced symlink to PageXML.h in py-textfeat.

--- py-pagexml/docker/Dockerfile_build.sh
+++ py-pagexml/docker/Dockerfile_build.sh
@@ -1,4 +1,8 @@
+#docker build -f Dockerfile_build -t mauvilsa/pagexml:build-ubuntu16.04-py27 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION= . &&
+docker build -f Dockerfile_build -t mauvilsa/pagexml:build-ubuntu16.04-py35 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION=3 . &&
+docker build -f Dockerfile_build -t mauvilsa/pagexml:build-ubuntu18.04-py36 --build-arg UBUNTU_TAG=18.04 --build-arg PYTHON_VERSION=3 .
+
 cd ../../
-docker build -f py-pagexml/docker/Dockerfile_build -t pagexml-build:16.04-py27 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION= . &&
-docker build -f py-pagexml/docker/Dockerfile_build -t pagexml-build:16.04-py35 --build-arg UBUNTU_TAG=16.04 --build-arg PYTHON_VERSION=3 . &&
-docker build -f py-pagexml/docker/Dockerfile_build -t pagexml-build:18.04-py36 --build-arg UBUNTU_TAG=18.04 --build-arg PYTHON_VERSION=3 .
+#docker build -f py-pagexml/docker/Dockerfile_test -t mauvilsa/pagexml:test-ubuntu16.04-py27 --build-arg BUILD_TAG=build-ubuntu16.04-py27 . &&
+docker build -f py-pagexml/docker/Dockerfile_test -t mauvilsa/pagexml:test-ubuntu16.04-py35 --build-arg BUILD_TAG=build-ubuntu16.04-py35 . &&
+docker build -f py-pagexml/docker/Dockerfile_test -t mauvilsa/pagexml:test-ubuntu18.04-py36 --build-arg BUILD_TAG=build-ubuntu18.04-py36 .
