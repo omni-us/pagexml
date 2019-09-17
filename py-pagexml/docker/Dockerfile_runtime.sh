a239b7e4cc7761a37bc66a74a132d13a544bb561
Mauricio Villegas <mauricio_ville@yahoo.com>
2019-09-17 10:22:23 +0200

    Improvements to runtime docker build

--- py-pagexml/docker/Dockerfile_runtime.sh
+++ py-pagexml/docker/Dockerfile_runtime.sh
@@ -1,11 +1,11 @@
 PAGEXML_VERSION=2019.07.08
 
-docker build \
-  -f Dockerfile_runtime \
-  -t mauvilsa/pagexml:runtime-ubuntu16.04-py27 \
-  --build-arg UBUNTU_TAG=16.04 \
-  --build-arg PYTHON_VERSION=2 \
-  --build-arg PAGEXML_VERSION=$PAGEXML_VERSION .
+#docker build \
+#  -f Dockerfile_runtime \
+#  -t mauvilsa/pagexml:runtime-ubuntu16.04-py27 \
+#  --build-arg UBUNTU_TAG=16.04 \
+#  --build-arg PYTHON_VERSION=2 \
+#  --build-arg PAGEXML_VERSION=$PAGEXML_VERSION .
 
 docker build \
   -f Dockerfile_runtime \
