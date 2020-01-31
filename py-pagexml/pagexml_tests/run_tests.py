eb503229feaaa1d9f856dd08d138f6301d60091d
Mauricio Villegas <mauricio_ville@yahoo.com>
2020-01-31 10:43:48 +0100

    - Command line runnable python scripts use python3 to be explicit. - Docker_build now includes libopenblas-dev and runs as non-root user.

--- py-pagexml/pagexml_tests/run_tests.py
+++ py-pagexml/pagexml_tests/run_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Run all unit tests in package."""
 
 import sys
--- py-textfeat/textfeat/bin/textfeats.py
+++ py-textfeat/textfeat/bin/textfeats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Command line tool for extracting text features."""
 
 import os
