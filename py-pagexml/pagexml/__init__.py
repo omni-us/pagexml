1ae5680255824072d90994cb6dd636d9102d8f6c
Mauricio Villegas <mauricio_ville@yahoo.com>
2022-03-02 08:33:51 +0100

    Bump version: 2021.07.28 → 2022.03.02

--- py-pagexml/pagexml/__init__.py
+++ py-pagexml/pagexml/__init__.py
@@ -7,7 +7,7 @@ except ImportError:
     cv2 = False
 
 
-__version__ = '2021.07.28'
+__version__ = '2022.03.02'
 
 PAGE_XSD_SEARCHINK = os.path.dirname(os.path.realpath(__file__))+'/xsd/pagecontent_searchink.xsd'
 PAGE_XSD_OMNIUS = os.path.dirname(os.path.realpath(__file__))+'/xsd/pagecontent_omnius.xsd'
--- py-textfeat/textfeat/__init__.py
+++ py-textfeat/textfeat/__init__.py
@@ -1,4 +1,4 @@
 from .swigTextFeatExtractor import *  # pylint: disable=import-error
 from .legacy import LegacyTextFeatExtractor
 
-__version__ = '2021.07.28'
+__version__ = '2022.03.02'
